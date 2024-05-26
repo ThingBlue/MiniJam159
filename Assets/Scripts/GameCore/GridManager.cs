1323f08e8e42a238dae40510ba88809eff44ace8
Bassil Virk <bassillooney@outlook.com>
2024-05-26 00:39:37 -0400

    Added IResource interface and modified GridManager to use CellTypes enum instead of bool

--- Assets/Scripts/GameCore/GridManager.cs
+++ Assets/Scripts/GameCore/GridManager.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 
 namespace MiniJam159.GameCore
 {
+    public enum CellType
+    {
+        Empty = 0,
+        Building = 1,
+        Resource = 2
+    }
+
     public class GridManager : MonoBehaviour
     {
         // Singleton
         public static GridManager instance;
 
-        public List<List<bool>> gridMatrix;
+        public List<List<CellType>> gridMatrix;
 
         private void Awake()
         {
@@ -20,13 +27,13 @@ namespace MiniJam159.GameCore
 
         private void Start()
         {
-            gridMatrix = new List<List<bool>>();
+            gridMatrix = new List<List<CellType>>();
             for (int i = 0; i < 20; i++)
             {
-                List<bool> newList = new List<bool>();
+                List<CellType> newList = new List<CellType>();
                 for (int j = 0; j < 20; j++)
                 {
-                    newList.Add(false);
+                    newList.Add(CellType.Empty);
                 }
                 gridMatrix.Add(newList);
             }
@@ -34,16 +41,21 @@ namespace MiniJam159.GameCore
 
         public bool isCellOccupied(int x, int y)
         {
-            return gridMatrix[y][x];
+            if (x < 0 || x >= gridMatrix[0].Count || y < 0 || y >= gridMatrix.Count)
+            {
+                throw new System.Exception("Invalid cell position");
+            }
+
+            return gridMatrix[y][x] != CellType.Empty;
         }
 
-        public void occupyCells(Vector2 startPosition, Vector2 size)
+        public void occupyCells(Vector2 startPosition, Vector2 size, CellType occupationType = CellType.Building)
         {
             for (int i = 0; i < size.x; i++)
             {
                 for (int j = 0; j < size.y; j++)
                 {
-                    gridMatrix[(int)startPosition.y + j][(int)startPosition.x + i] = true;
+                    gridMatrix[(int)startPosition.y + j][(int)startPosition.x + i] = occupationType;
                 }
             }
         }
