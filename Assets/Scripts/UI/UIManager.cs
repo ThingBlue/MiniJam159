56226f71fabd3af8988d077ae692955355f928e6
ThingBlue <thingbluex@gmail.com>
2024-05-26 22:16:45 -0400

    Display for multi-selected units

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -136,16 +136,43 @@ namespace MiniJam159.UI
             if (selectedObjects.Count == 1 && selectedObjects[0].GetComponent<Structure>())
             {
                 GameObject newDisplayBox = Instantiate(displayBoxPrefab, displayPanel.transform);
-                newDisplayBox.GetComponent<RectTransform>().localPosition = new Vector3(0, displayCenterHeight, 0);
+                newDisplayBox.GetComponent<RectTransform>().localPosition = new Vector3(0f, displayCenterHeight, 0f);
                 newDisplayBox.GetComponent<Image>().sprite = selectedObjects[0].GetComponent<Structure>().structureData.displaySprite;
                 displayBoxes.Add(newDisplayBox);
                 return;
             }
 
             // Units
+            int columns = Mathf.FloorToInt(displayPanel.GetComponent<RectTransform>().sizeDelta.x / 32.0f);
+            int rows = Mathf.CeilToInt((float)selectedObjects.Count / (float)columns);
+            int lastRowColumns = selectedObjects.Count % columns;
+            if (lastRowColumns == 0) lastRowColumns = columns;
+            float leftBoxXPosition = 0f - (columns - 1) * 32f / 2f;
+            float lastRowLeftBoxXPosition = 0f - (lastRowColumns - 1) * 32f / 2f;
+
             for (int i = 0; i < selectedObjects.Count; i++)
             {
                 GameObject selectedUnit = selectedObjects[i];
+
+                Vector3 boxLocalPosition = new Vector3(0f, 0f, 0f);
+                if (i < selectedObjects.Count - lastRowColumns)
+                {
+                    // Not last row
+                    boxLocalPosition.x = leftBoxXPosition + (i % columns) * 32f;
+                }
+                else
+                {
+                    // Last row
+                    boxLocalPosition.x = lastRowLeftBoxXPosition + (i % columns) * 32f;
+                }
+                //boxLocalPosition.y = displayCenterHeight - Mathf.Floor(i + 1 / (float)columns) * 32f;
+                int currentRow = Mathf.FloorToInt(i / columns);
+                boxLocalPosition.y = displayCenterHeight - currentRow * 32f;
+
+                GameObject newDisplayBox = Instantiate(displayBoxPrefab, displayPanel.transform);
+                newDisplayBox.GetComponent<RectTransform>().localPosition = boxLocalPosition;
+                newDisplayBox.GetComponent<Image>().sprite = selectedObjects[0].GetComponent<GameAI>().displaySprite;
+                displayBoxes.Add(newDisplayBox);
             }
         }
 
